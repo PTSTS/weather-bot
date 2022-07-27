761ba32a1b0cae7e3e3905ea116a4be60de9450f
Peter Santos <AIMEPTSTSAM@mail.com>
2022-07-28 00:10:59 +0200

    Server completed

--- server/app.py
+++ server/app.py
@@ -28,17 +28,35 @@ def check_weather():
     session_name = request_body['sessionInfo']['session']
     date = datetime.date(int(date_dict['year']), int(date_dict['month']), int(date_dict['day']))
     location_list = request_body['sessionInfo']['parameters']['location']
-    location = ', '.join(location_list)
-    print(request_body)
+    if type(location_list) is list:
+        location = ', '.join(location_list)
+    else:
+        location = location_list
     day_weather = weather(location, date, api_key)
-    if day_weather.location_found:
+    if day_weather.location_found and day_weather.date_available:
         response = fulfillment_response(
-            f"""You aksed for the weather on {date.day}/{date.month}/{date.year} in the location 
-{day_weather.location}. The lowest temperature is {day_weather.low_temp} C, the highest temperature is 
-{day_weather.high_temp}. Is this the right location you are looking for?""",
+            f"""You asked for the weather on {date.day}/{date.month}/{date.year} in the location:
+{day_weather.location}. \nThe lowest temperature is {day_weather.low_temp} C, the highest temperature is 
+{day_weather.high_temp} C. Is this the right location you are looking for?""",
             parameters={'location_validated': True}, session=session_name
         )
-        
+    elif day_weather.location_found and not day_weather.date_available:
+        response = fulfillment_response(
+            f"""Sorry, we don't have the forecast on this day for this location, can you try an earlier date?""",
+            parameters={'location_validated': True, 'date_validated': False, 'date': None}, session=session_name
+        )
+    elif not day_weather.location_found:
+        response = fulfillment_response(
+            f"""We couldn't find this location "{location}".""",
+            parameters={'location_validated': False, 'location': None}, session=session_name
+        )
+    else:
+        response = fulfillment_response(
+            f"""Sorry, something went wrong. {day_weather.error_message}.""",
+            parameters={'location_validated': False}, session=session_name
+        )
+
+    return make_response(jsonify(response))
 
 
 @app.route('/date', methods=['GET', 'POST'])
@@ -53,7 +71,7 @@ def check_date():
 
     if date - current_date > datetime.timedelta(days=10):
         response = fulfillment_response(
-            """Sorry, we can only provide forecast for the next 14 days.""",
+            """Sorry, we can only provide forecast for the next 10 days.""",
             parameters={'date_validated': False, 'date': None}, session=session_name
         )
     elif date < datetime.date.today():
--- server/weather.py
+++ server/weather.py
@@ -10,6 +10,7 @@ url = 'https://api.weatherapi.com/v1/'
 class Weather:
     def __init__(self):
         self.location_found = False
+        self.date_available = False
         self.high_temp = -273
         self.low_temp = -273
         self.error = False
@@ -19,7 +20,7 @@ class Weather:
 
 def weather(location: str, date, api_key):
     current_date = datetime.date.today()
-    location = location.replace('&', '')  # '&' is not allowed for API call
+    location = location.replace('&', '')
 
     data = {
         'key': api_key,
@@ -29,9 +30,10 @@ def weather(location: str, date, api_key):
         'alerts': 'no',
     }
 
-    request_url = url + 'forecast.json?' + '&'.join(f'{key}={value}' for key, value in data.items())
+    request_url = url + 'forecast.json?' + '&'.join(f"{key}={value}" for key, value in data.items())
     response = requests.get(request_url)
     response_body = json.loads(response.text)
+    print(request_url)
 
     weather_data = Weather()
 
@@ -42,15 +44,19 @@ def weather(location: str, date, api_key):
             if 'message' in response_body['error'].keys():
                 weather_data.error_message = response_body['error']['message']
             else:
-                weather_data.error_message = 'Something else went wrong'
+                weather_data.error_message = 'Weather API error.'
         else:
-                weather_data.error_message = 'Something else went wrong'
+                weather_data.error_message = 'Weather API error.'
+    elif len(response_body["forecast"]['forecastday']) < data['days']:
+        weather_data.location_found = True
+        weather_data.date_available = False
     else:
         high_temp, low_temp = extract_temp(response_body, date)
         weather_data.location_found = True
+        weather_data.date_available = True
         weather_data.high_temp = high_temp
         weather_data.low_temp = low_temp
-        weather_data.location = ','.join([
+        weather_data.location = ', '.join([
             response_body['location']['name'],
             response_body['location']['region'],
             response_body['location']['country'],
